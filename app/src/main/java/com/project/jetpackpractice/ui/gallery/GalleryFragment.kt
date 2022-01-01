d7ff69b67d4878d5bc166d7d9a5060e9056d8fb7
wsb7788 <wsb0514@naver.com>
2022-01-01 23:43:32 +0900

    nav graph 기본 설정 및 연결

---
+++ app/src/main/java/com/project/jetpackpractice/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,8 @@
+package com.project.jetpackpractice.ui.gallery
+
+import androidx.fragment.app.Fragment
+import com.project.jetpackpractice.R
+
+class GalleryFragment: Fragment(R.layout.fragment_gallery) {
+
+}
